304e3c7dfe6aa3a668a1837346816e9b2d5dd0d8
Paul Tuckett <paul.tuckett@xenetixsoftware.com>
2021-05-04 07:20:43 -0600

    Applied async to connections

--- postgres.py
+++ postgres.py
@@ -1,44 +1,56 @@
-import psycopg2
+import asyncio
+import asyncpg
 import json
 import os
 
 
-class JSONPostGresData:
+class JSONPostGresData(object):
     def __init__(self):
         db_file = open(os.environ["DB_SECRET"], "r+")
         data = json.load(db_file)
         self.query = data.pop("query")
         self.db = data
 
-    def connect(self):
-        try:
-            self.conn = psycopg2.connect(**self.db).cursor()
-        except (Exception, psycopg2.DatabaseError) as error:
-            self.conn = None
-            print(error)
+    # async def _init(self):
+    #     self.pool = await create_pool()
 
-    def get_items(self, query):
-        self.conn.execute(query)
-        return self.conn.fetchall()
+    async def connect(self):
+        try:
+            self.conn = await asyncpg.connect(**self.db)
+            print(self.conn)
+        except Exception as error:
+            await conn.close()
+
+    async def get_items(self, query):
+        all_items = await self.conn.fetch(query)
+        return all_items
 
 
 selection = input("Write new query or from db file (new, db): ")
 
-c = JSONPostGresData()
-c.connect()
-if c.conn is not None:
-    if selection == "new":
-        query = input("Write query here: ")
-    elif selection == "db":
-        query = c.query
-    else:
-        query = None
 
-    if query:
-        items = c.get_items(query)
+async def main(query):
+    c = JSONPostGresData()
+    # c = await connect_db()
+    await c.connect()
+
+    if c.conn is not None:
+        if selection == "new":
+            query = input("Write query here: ")
+        elif selection == "db":
+            query = c.query
+        else:
+            query = None
+
+        if query:
+            items = await c.get_items(query)
+        else:
+            print("Need to pass a query in.")
+        for item in items:
+            print(item[0])
     else:
-        print("Need to pass a query in.")
-    for item in items:
-        print(item[0])
-else:
-    print("Error connecting")
+        print("Error connecting")
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main(selection))
